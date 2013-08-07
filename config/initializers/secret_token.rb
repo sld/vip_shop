d7ad9720caef54c3504c548ae502cc269bf87bc9
Idris <sld7700@gmail.com>
2013-08-07 18:23:13 +0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VipShop::Application.config.secret_key_base = '4b691f7495f4272cd3c64e3176011847c61fc59b9bfc73f99ba9f3f4741453195db56adfa1a4feebec9016c2ce0f2eaa617aa548cd29a1f2d52f7111959c4abe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VipShop::Application.config.session_store :cookie_store, key: '_vip_shop_session'
