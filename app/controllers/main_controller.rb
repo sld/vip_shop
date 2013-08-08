1762396f7c72ed9a47a50ea44991d5dfce6c746b
Idris <sld7700@gmail.com>
2013-08-08 17:11:05 +0600

    Changed build_cart to create_cart in MainController

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,6 +9,6 @@ class MainController < ApplicationController
 
 
   def build_user_cart
-    current_user.cart || current_user.build_cart 
+    current_user.cart || current_user.create_cart 
   end
 end
