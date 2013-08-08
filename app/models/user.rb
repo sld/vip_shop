4cbbdffb4a1e8cb00aca45463fb491ad9202e5f0
Idris <sld7700@gmail.com>
2013-08-08 12:06:52 +0600

    Deleted recoverable from User's devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 
   has_one :cart
   has_many :buyings
