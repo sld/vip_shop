a0e3fc620c4549444069631aab23edfc3b308691
Idris <sld7700@gmail.com>
2013-08-08 17:57:20 +0600

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20130807181936) do
   create_table "cart_products", force: true do |t|
     t.integer  "cart_id"
     t.integer  "product_id"
-    t.integer  "quantity"
+    t.integer  "quantity",   default: 1
     t.datetime "created_at"
     t.datetime "updated_at"
   end
