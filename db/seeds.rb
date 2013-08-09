1ea58e46543b5f4678455fd59238365a7b6f7b8d
Idris <sld7700@gmail.com>
2013-08-10 00:26:13 +0600

    Updated db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create :email => "email@example.com", :password => "password", :password_confirmation => "password"
-User.create :email => "email2@example.com", :password => "password", :password_confirmation => "password"
+usr1 = User.create :email => "email@example.com", :password => "password", :password_confirmation => "password", :balance => 10000
+usr2 = User.create :email => "email2@example.com", :password => "password", :password_confirmation => "password", :balance => 10000
+usr3 = User.create :email => "email3@example.com", :password => "password", :password_confirmation => "password", :balance => 0
 
 20.times{ |i| Product.create( :name => "Product-#{i}", :price => 500 + rand(1500) ) }
+
+# Create 8 Buyings
+4.times do |i| 
+  2.times{ |j| usr1.cart.products << Product.all[i+j] }
+  2.times{ |j| usr2.cart.products << Product.all[4+j+i]}
+  usr1.buy_cart!
+  usr2.buy_cart!
+end
+
+
+
